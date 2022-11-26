b3c5b3080d51354d484b2c3ee2fc83e43e09e058
3xt1ncion <meln1kbtw@gmail.com>
2022-11-26 21:50:29 +0300

    1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,56 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("Задание 1: ")
+    println()
+    ProgramOne_1()
+    println()
+    ProgramOne_2()
+}
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun ProgramOne_1() {
+    println("1) Разработать программу, которая для заданной строки s " +
+            "подсчитывает общее число вхождений символов х и у")
+    println()
+    print("Введите строку: ")
+    val s = readLine()
+    print("Введите первый символ: ")
+    val x = readLine()!![0]
+    print("Введите второй символ: ")
+    val y = readLine()!![0]
+    var counterx = 0
+    var countery = 0
+    var n = 0
+    s!!.forEach { _ ->
+        val char = s[n]
+        if (char == x) counterx++
+        if (char == y) countery++
+        n++
+    }
+    println("Кол-во символов '$x': $counterx; Кол-во символов '$y': $countery")
+}
+
+fun ProgramOne_2() {
+    println("2) Разработать программу, которая для заданной строки s " +
+            "определяет, какой из двух заданных символов встречается чаще в строке")
+    println()
+    print("Введите строку: ")
+    val s = readLine()
+    print("Введите первый символ: ")
+    val x = readLine()!![0]
+    print("Введите второй символ: ")
+    val y = readLine()!![0]
+    var counterx = 0
+    var countery = 0
+    var n = 0
+    s!!.forEach { _ ->
+        val char = s[n]
+        if (char == x) counterx++
+        if (char == y) countery++
+        n++
+    }
+    if (counterx > countery) {
+        println("Символ '$x' употребляется чаще ($counterx > $countery)")
+    }
+    else {
+        println("Символ '$y' употребляется чаще ($countery > $counterx)")
+    }
 }
